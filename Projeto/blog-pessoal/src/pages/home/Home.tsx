c194b4b516de79ce8b3c171b9e6ddf02eec93cf1
Evandro <eesvans@hotmail.com>
2022-02-14 12:37:03 -0300

    Atividade React

---
+++ Projeto/blog-pessoal/src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="http://pm1.narvii.com/6600/58340b5d07b2f1ea119ff95c6e18d816284218a2_00.jpg" alt="Imagem incial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
